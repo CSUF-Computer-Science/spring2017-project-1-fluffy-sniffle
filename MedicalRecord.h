91fee8e85b794c9f929d462322439dff5bed8aa8
tonyngocpham <tonyngocpham@csu.fullerton.edu>
2017-03-01 22:27:16 -0800

    Update MedicalRecord.h

--- MedicalRecord.h
+++ MedicalRecord.h
@@ -6,20 +6,25 @@
 
 using namespace std;
 
+
+
+
 class MedicalRecord {
 public:
-	// default constructor
+
 	MedicalRecord() {
-		// TO BE COMPLETED
+		babies = new Baby[1000000];	
+		numofbabies = 0;	
+
 	}
 
-	// destructor
+	
 	~MedicalRecord() {
-		// TO BE COMPLETED
+		delete[] babies;	
 	}
 
-	// Load information from a text file with the given filename.
-	void buildMedicalRecordfromDatafile(string filename) {
+	
+	void buildMedicalRecordfromDatafile(string filename) {		
 		ifstream myfile(filename);
 
 		if (myfile.is_open()) {
@@ -27,7 +32,7 @@ public:
 			string name;
 			int weight;
 			while (myfile >> name >> weight) {
-				// cout << name << " " << weight << endl;
+				
 				Baby b(name, weight);
 				addEntry(b);
 			}
@@ -37,33 +42,95 @@ public:
 			throw invalid_argument("Could not open file " + filename);
 	}
 
-	// return the most frequently appearing name in the text file
-	string mostPopularName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
+	string mostPopularName() {		
+									
+		string names[1000];	
+		for (int i = 0; i < 1000; i++) 
+		{
+			names[i] = "_";
+		}
+
+		cout << "Making the names array." << endl;	
+		int inc = 0;	
+		int totNames = 0;	
+		for (int i = 0; i < numofbabies; i++)
+		{
+			for (int x = 0; x < 1000; x++)	
+			{
+				
+				
+				if (babies[i].name == names[x])
+				{
+					break; 
+						   
+				}
+				else if (names[x] == "_")
+				{
+					names[x] = babies[i].getName();	
+					totNames++; 
+					break;
+				}
+			}
+			inc++;	
+			if (inc % 10000 == 0)	
+				cout << inc << endl;	
+		}
+		cout << "Passed making the names array." << endl;	
+		int popNameIndex;	
+		int popNameCount = 0;	
+		inc = 0;		
+		for (int i = 0; i < 1000; i++)	
+		{
+			int currentNum = numberOfBabiesWithName(names[i]);	
+			if (currentNum > popNameCount) 
+			{
+				popNameIndex = i;	
+				popNameCount = currentNum;
+			}
+			inc++;	
+			if (inc % 10 == 0)	
+				cout << inc << endl;	 
+		}
+		return names[popNameIndex];	
+									
 	}
 
-	// return the number of baby records loaded from the text file
+	
 	int numberOfBirths() {
-		return -1; // TO BE COMPLETED
+		return numofbabies;	
 	}
 
-	// return the number of babies who had birth weight < 2500 grams
+
 	int numberOfBabiesWithLowBirthWeight() {
-		return -1; // TO BE COMPLETED
+		int lowWeightCount = 0;	
+		for (int i = 0; i < numofbabies; i++)	
+		{
+			if (babies[i].getWeight() < 2500)
+				lowWeightCount++;
+		}
+		return lowWeightCount;	
 	}
 
-	// return the number of babies who have the name contained in string s
-	int numberOfBabiesWithName(string s) {
-		return -1; // TO BE COMPLETED
+
+	int numberOfBabiesWithName(string s) {					
+		int numBabiesWithName = 0;				
+		for (int i = 0; i < numofbabies; i++)			
+		{
+			if (babies[i].getName() == s)
+				numBabiesWithName++;
+		}
+		return numBabiesWithName;
 	}
 
 private:
-	// update the data structure with information contained in Baby object
-	void addEntry(Baby b) {
-		// TO BE COMPLETED
+
+	void addEntry(Baby b) {		
+		babies[numofbabies] = b;	
+		numofbabies++;				
 	}
 
-	// Add private member variables for your data structure along with any 
-	// other variables required to implement the public member functions
+	
+	Baby * babies;	
+	int numofbabies;			
 
 };
